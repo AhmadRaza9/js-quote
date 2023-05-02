7e3f149d178e128529641cba9cf0c0207bd28829
AhmadRaza9 <raza.atakiu@gmail.com>
2023-05-02 17:53:56 +0500

    convert code into async function

--- script.js
+++ script.js
@@ -4,13 +4,36 @@ const button = document.querySelector("button");
 const author = document.querySelector(".author");
 const quote = document.querySelector(".quote");
 
-button.addEventListener("click", () => {
-  fetch(api)
-    .then((res) => res.json())
-    .then((data) => {
-      console.log(data);
-      author.innerText = data.author;
-      quote.innerText = data.content;
-    })
-    .catch(alert("Error fetching quote"));
-});
+ 
+
+// OLD CODE
+
+// button.addEventListener("click", async () => {
+//   await fetch(api)
+//     .then((res) => res.json())
+//     .then((data) => {
+//       console.log(data);
+//       author.innerText = data.author;
+//       quote.innerText = data.content;
+//     })
+//     .catch(alert("Error fetching quote"));
+// });
+
+// OLD CODE
+
+async function getData() {
+  try {
+    const response = await fetch(api);
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    const data = await response.json();
+    author.innerText = data.author;
+    quote.innerText = data.content;
+  } catch (error) {
+    console.error("Error:", error);
+    alert("Error fetching quote!");
+  }
+}
+
+button.addEventListener("click", getData);
